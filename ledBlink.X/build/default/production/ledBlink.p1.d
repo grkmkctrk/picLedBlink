bb851d5f95281c223f13c1b1db6aadc37007db3b
grkmkctrk <65461375+grkmkctrk@users.noreply.github.com>
2020-12-13 13:51:44 +0300

    Add files via upload

---
+++ ledBlink.X/build/default/production/ledBlink.p1.d
@@ -0,0 +1,3 @@
+build/default/production/ledBlink.p1:  \
+ledBlink.c  \
+ledBlink.h 
---
+++ ledBlink.X/ledBlink.h
@@ -0,0 +1,22 @@
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef XC_HEADER_TEMPLATE_H
+#define	XC_HEADER_TEMPLATE_H
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    // TODO If C++ is being used, regular C code needs function names to have C 
+    // linkage so the functions can be used by the c code. 
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
+
+void ledAyarla(void);
+void ledBlink(void);
